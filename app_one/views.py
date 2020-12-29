c3410b202bd8371ceb1d1aa747400e02369aad37
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2020-12-29 10:03:31 -0600

    Update list of dojos and ninjas

--- app_one/views.py
+++ app_one/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import *
 
@@ -11,3 +11,17 @@ def index(request):
         "ninja": Ninja.objects.all(),
     }
     return render(request, 'index.html', context)
+
+def add_dojo(request):
+    if request.method == 'GET':
+        return redirect('/')
+    if request.method == 'POST':
+        Dojo.objects.create(name=request.POST['name'],city=request.POST['city'],state=request.POST['state'],desc=request.POST['desc'])
+        return redirect('/')
+
+def add_ninja(request):
+    if request.method == 'GET':
+        return redirect('/')
+    if request.method == 'POST':
+        Ninja.objects.create(dojo=Dojo.objects.get(name=request.POST['dojo']),first_name=request.POST['first_name'],last_name=request.POST['last_name'])
+        return redirect('/')
