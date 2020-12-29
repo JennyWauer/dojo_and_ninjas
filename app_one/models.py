bcdf08df550d21da07455dc3ae9c6dbf3abd6687
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2020-12-28 20:10:15 -0600

    add dojo model

--- app_one/models.py
+++ app_one/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Dojo(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=2)
